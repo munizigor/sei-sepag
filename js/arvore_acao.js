69c503ab0bce7c678fdd5916ab2a3021294d7e53
¨Igor <muniz.igor@gmail.com>
2021-02-21 10:00:14 -0300

    Opcoes de doc na aba de edicao

--- js/arvore_acao.js
+++ js/arvore_acao.js
@@ -19,21 +19,39 @@ function clicar_documento (indice) {
 }
 
 function criar_botao_sepag () {
-    document.getElementById("ifrVisualizacao").onload = function() {
-        var visualizacao = document.getElementById("ifrVisualizacao").contentWindow.document;
-        var botaoSEPAG = visualizacao.createElement("a");
-        botaoSEPAG.setAttribute("class","botaoSEI");
-        botaoSEPAG.innerHTML = '<img class="infraCorBarraSistema" alt="Inserir Documentos SEPAG" title="Inserir Documentos SEPAG">'
-        imgSEPAG = botaoSEPAG.childNodes[0];
-        imgSEPAG.setAttribute("src",chrome.runtime.getURL("img/icon_cbm.jpeg"));
-        visualizacao.getElementById("divArvoreAcoes").appendChild(botaoSEPAG);
-    }
+//    window.onload = function() {
+//        var visualizacao = document.getElementById("ifrVisualizacao").contentWindow.document;
+        console.log(document.readyState)
+        console.log(document.body.innerHTML)
+        var botaoSEPAG = document.createElement("div");
+//        botaoSEPAG.setAttribute("class","cke_button");
+        botaoSEPAG.setAttribute("id","docsSEPAG");
+        botaoSEPAG.setAttribute("class","cke_top");
+//        botaoSEPAG.innerHTML = '<img alt="Inserir Documentos SEPAG" title="Inserir Documentos SEPAG">'
+//        var imgSEPAG = botaoSEPAG.childNodes[0];
+//        imgSEPAG.setAttribute("src",chrome.runtime.getURL("img/icon_cbm.jpeg"));
+
+botaoSEPAG.innerHTML = `
+                        <div>
+                          <label for="tipo_doc">Documentos SEPAG</label>
+                          <select id="tipo_doc">
+                             <option value=""></option>
+                             <option value="DIARIAS_plan_calc">DIÁRIAS - Planilha de Cálculo</option>
+                             <option value="RRM_fard">RRm - Fardamento por passagem pra RRm</option>
+                             <option value="RECON_calculo">RECONHECIMENTO DE DÍVIDA - Cálculo</option>
+                             <option value="RESS_calculo">RESSARCIMENTO AO ERÁRIO - Cálculo</option>
+                             <option value="CEDIDO_calculo">PESSOAL CEDIDO - Cálculo Ressarcimento</option>
+                             <option value="DEP_calculo_acerto">DEPENDENTES - Cálculo de Valores de Acerto em Folha</option>
+                             <option value="GRAT_calculo_acerto">GRATIFICAÇÕES - Cálculo de Valores de Acerto em Folha</option>
+                          </select>
+                        </div>
+`
+
+        document.getElementById("divComandos").insertAdjacentElement('afterend',botaoSEPAG);
+//    }
 }
+//alert('ok');
 criar_botao_sepag ();
 
-/*
-TODO: html botao SEI
-<a class="botaoSEI">
-<img class="infraCorBarraSistema" src="imagens/sei_controle_processos.gif" alt="Controle de Processos" title="Controle de Processos">
-</a>
-*/
+//TODO: buscar dados para POST
+
